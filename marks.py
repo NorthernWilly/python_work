8fb51648fde4a3c78ccb1dd911e6d7791943823e
NorthernWilly <ianwilson2791@gmail.com>
2019-05-24 10:19:45 +0100

    first python programs

---
+++ marks.py
@@ -0,0 +1,6 @@
+Physics=80
+Chemistry=94
+Math=57
+Total=Physics+Chemistry+Math
+print("The Result is")
+print(Total)
---
+++ marksper.py
@@ -0,0 +1,12 @@
+Physics=80
+Chemistry=100
+Math=100
+Total=Physics+Chemistry+Math
+Per=Total*100/450
+print("Physics:",Physics)
+print("Chemistry:",Chemistry)
+print("Math:",Math)
+print("---------------------")
+print("Total Marks:",Total)
+print("Percentage",Per)
+print("---------------------")
