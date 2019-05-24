a3165300c82d34cb18ac00631750e061eb581446
NorthernWilly <ianwilson2791@gmail.com>
2019-05-24 14:17:08 +0100

    nested ifs

---
+++ amounts.py
@@ -0,0 +1,10 @@
+No = int(input("Enter Number"))
+if No>1000:
+	print("b")
+	if No>2000:
+		print("s")
+else:
+	print("a")
+	if No>500:
+		print("h")
+
---
+++ amounts2.py
@@ -0,0 +1,16 @@
+No = int(input("Enter number:"))
+if No>1000:
+	print("hey you are")
+	if No>2000:
+		print("baller")
+	else:
+		print("decent")
+	print("congrats")
+else:
+	print("we need to talk")
+	if No>500:
+		print("start worrying a bit")
+	else:
+		print("son you are broke")
+	print("do you understand?")
+print("How does that make you feel?")
